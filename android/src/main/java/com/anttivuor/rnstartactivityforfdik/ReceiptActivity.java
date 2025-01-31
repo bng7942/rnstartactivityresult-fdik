4a4436d095786b8c65c9941d976dec036d4a4f60
unknown <bng49890@gmail.com>
2025-01-31 10:20:02 +0900

    added ReceiptActivity

--- android/src/main/java/com/anttivuor/rnstartactivityforfdik/ReceiptActivity.java
+++ android/src/main/java/com/anttivuor/rnstartactivityforfdik/ReceiptActivity.java
@@ -25,7 +25,7 @@ public class ReceiptActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_receipt);
+        // setContentView(R.layout.activity_receipt);
         mContext = this;
         CallbackDataProcess();
     }
